3e313ad2d4aae08bc89a62bfc5b00319cee7ec9e
RedaRasmy <red7mer@gmail.com>
2024-10-25 21:50:36 +0100

    save

--- src/components/settings/Clickable.tsx
+++ src/components/settings/Clickable.tsx
@@ -1,6 +1,6 @@
 import useCard from '@/library/Hooks/useCard'
 import { useAppSelector } from '@/library/redux/store'
-import React, { ReactNode, use } from 'react'
+import { ReactNode } from 'react'
 
 export default function Clickable({children,id}:{children:ReactNode,id:number}) {
     const isClickable = useAppSelector(state=>state.settings.clicking)
