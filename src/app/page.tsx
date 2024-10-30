4294124a79f32dbd7822a4f70eb1da2d0c437b51
RedaRasmy <red7mer@gmail.com>
2024-10-30 20:54:31 +0100

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import MainMenu from "@/components/Home/MainMenu";
-import Draggable from "@/library/dnd-kit/draggable";
+// import Draggable from "@/library/dnd-kit/draggable";
 import { DndContext } from "@dnd-kit/core";
 
 export default function Home() {
@@ -10,11 +10,11 @@ export default function Home() {
         <DndContext>
             <div className="flex flex-col justify-around items-center h-full overflow-hidden">
                     <MainMenu/>
-                    <Draggable id={0}>
+                    {/* <Draggable id={0}>
                         <div className="w-52 bg-white h-52">
                             hello
                         </div>
-                    </Draggable>
+                    </Draggable> */}
                 </div>
         </DndContext>
     );
