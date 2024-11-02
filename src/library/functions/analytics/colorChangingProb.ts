2edf6c682ec3a7a4421d3a64e837fff034ec8679
RedaRasmy <red7mer@gmail.com>
2024-11-03 00:34:50 +0100

    save

--- src/library/functions/analytics/colorChangingProb.ts
+++ src/library/functions/analytics/colorChangingProb.ts
@@ -1,19 +1,24 @@
 import { cards } from "../../../../public/cards"
+import C from "./combination"
 
 
 export default function colorChangingProb(CTP:number,KC:number[],ECN:number) {
     let p1 = 0 , p2 = 0
+    const UN = 40 - KC.length
     const JudgeCardWithCTPcolor = cards.filter(
         card=>((card.power === 'JUDGE') && (card.color === cards[CTP].color))
     )[0].id
-    if (!KC.includes(JudgeCardWithCTPcolor)){
-        p1 = ECN/(40-KC.length)*0.75
+    if (!KC.includes(JudgeCardWithCTPcolor)){ 
+        // p1 = ECN/(40-KC.length)*0.75
+        p1 = (1 - C(UN-1,ECN)/C(UN,ECN))*0.75
     }
     if (cards[CTP].number) {
         const Exception = KC.filter(
             card=>( (cards[card].number === cards[CTP].number) && (card!==CTP) )
         ).length
-        p2 = ECN/(40-KC.length)*(3-Exception)
+        // p2 = ECN/(40-KC.length)*(3-Exception)
+        const n = UN - (3-Exception)
+        p2 = 1 - C(n,ECN)/C(UN,ECN)
     }
     return p1 + p2
 }
--- src/library/functions/analytics/numberChangingProb.ts
+++ src/library/functions/analytics/numberChangingProb.ts
@@ -1,11 +1,14 @@
 import { cards } from "../../../../public/cards"
+import C from "./combination"
 
 
 export default function numberChangingProb(CTP:number,KC:number[],ECN:number) {
     const UC = 40 - KC.length
     const color = cards[CTP].color
     const Exception = KC.filter(card=>((cards[card].color === color)&&(card!==CTP))).length
-    return (ECN/UC)*(9-Exception)
+    // return (ECN/UC)*(9-Exception)
+    const n = UC - (9-Exception)
+    return 1 - C(n,ECN)/C(UC,ECN)
 }
 
 
