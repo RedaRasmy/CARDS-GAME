f07aa37a537c7763ec785cfba641514ca6154146
RedaRasmy <red7mer@gmail.com>
2024-12-05 22:43:21 +0100

    fix build probs

--- src/library/Hooks/useProba.ts
+++ src/library/Hooks/useProba.ts
@@ -1,21 +1,21 @@
-import colorChangingProb from "../functions/analytics/colorChangingProb";
-import getKnownCards from "../functions/analytics/getKnownCards";
-import numberChangingProb from "../functions/analytics/numberChangingProb";
-import playProb from "../functions/analytics/playProb";
-import { useAppSelector } from "../redux/store";
+// import colorChangingProb from "../functions/analytics/colorChangingProb";
+// import getKnownCards from "../functions/analytics/getKnownCards";
+// import numberChangingProb from "../functions/analytics/numberChangingProb";
+// import playProb from "../functions/analytics/playProb";
+// import { useAppSelector } from "../redux/store";
 
-export default function useProba(CTP:number) {
-    const turns = useAppSelector(state=>state.gameFlow.turns)
-    const {playerCards,currentCardId,botCards} = useAppSelector(state=>state.cardsFlow)
-    const KC = getKnownCards(turns,playerCards,currentCardId)
-    const playProba = parseFloat(playProb(CTP,KC,botCards.length).toFixed(2))*100
-    const numberChangeProba = parseFloat(numberChangingProb(CTP,KC,botCards.length).toFixed(2))*100
-    const colorChangeProba = parseFloat(colorChangingProb(CTP,KC,botCards.length).toFixed(2))*100
+// export default function useProba(CTP:number) {
+//     const turns = useAppSelector(state=>state.gameFlow.turns)
+//     const {hands,currentCardId,} = useAppSelector(state=>state.cardsFlow)
+//     const KC = getKnownCards(turns,hands[0],currentCardId)
+//     const playProba = parseFloat(playProb(CTP,KC,botCards.length).toFixed(2))*100
+//     const numberChangeProba = parseFloat(numberChangingProb(CTP,KC,botCards.length).toFixed(2))*100
+//     const colorChangeProba = parseFloat(colorChangingProb(CTP,KC,botCards.length).toFixed(2))*100
 
-    return {
-        playProba,
-        numberChangeProba,
-        colorChangeProba
-    }
-}
+//     return {
+//         playProba,
+//         numberChangeProba,
+//         colorChangeProba
+//     }
+// }
 
